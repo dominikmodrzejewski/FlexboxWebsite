c9df489adbe94384a17652077420802a912cda6a
Dominik Modrzejewski <dominikmodrzejewski@pw.pl>
2021-09-13 22:18:48 +0200

    created islands section

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-const nav = document.querySelector(".nav");
-const navBtn = document.querySelector(".burger-btn");
-const allNavItems = document.querySelectorAll(".nav__item");
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+
 
 const handleNav = () => {
-  nav.classList.toggle("nav--active");
+    nav.classList.toggle('nav--active')
 
-  allNavItems.forEach(item => {
-    item.addEventListener("click", () => {
-      nav.classList.remove("nav--active");
-    });
-  });
-  handleNavItemAnimation();
-};
+    navBtnBars.classList.remove('black-bars-color');
 
-const handleNavItemAnimation = () => {
-  let delayTime = 0;
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav--active')
+        })
+    })
 
-  allNavItems.forEach(item => {
-    item.classList.toggle("nav-items-animation");
-    item.style.animationDelay = "." + delayTime + "s";
-    delayTime++;
-  })
+    handleNavItemsAnimation();
 }
 
-navBtn.addEventListener("click", handleNav);
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+navBtn.addEventListener('click', handleNav);
